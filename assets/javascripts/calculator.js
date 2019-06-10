58e2aef624783d22ce11fcfc6c6c80b9f69e4a83
soheila <soheilalotfi@ymail.com>
2019-06-10 08:10:13 -0400

    Set the HTML of the .number to the text of what was clicke for the second number

--- assets/javascripts/calculator.js
+++ assets/javascripts/calculator.js
@@ -3,28 +3,41 @@ $(document).ready(function(){
 //creating variables
 
 var firstNumber="";
-
+var isOperatorClicked=false;
+var secondNumber="";
 
 // getting user first number
 
 $(".number").click(function(){
-
-    var valNumber=$(this).val();
-    firstNumber+= valNumber;
-    $("#firstnumber").text(firstNumber);
-
+        if(isOperatorClicked==false){
+            var valNumber=$(this).val();
+            firstNumber+= valNumber;
+            $("#firstnumber").text(firstNumber);
+        }
 
 });
 
+//Set the HTML of the #operator to the text of what was clicked
 $(".operator").click(function(){
 
 var valCalculator=$(this).text();    // dont forget to put parenthesis infront of text-text()
 $("#operator").text(valCalculator);
+isOperatorClicked=true;
+});
+
+//Set the HTML of the .number to the text of what was clicked
+
+$(".number").click(function(){
+        if (isOperatorClicked==true){             //isOperatorClicked variable helps to set the html of the firstnumber , operator and secondnumber in order.
+            var valNumber1=$(this).val();
+            secondNumber+= valNumber1;
+            $("#secondnumber").text(secondNumber);
+        }
+   
+
+});
 
 
-
-
-})
 
 
 });
